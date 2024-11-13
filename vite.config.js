de95cad4523f26482c108ab11c895b8f0110b6f0
Aashish Karki <078bct004.aashish@pcampus.edu.np>
2024-11-13 10:57:20 +0545

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,31 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+
+// Define __dirname for ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/npl-coder-frontend/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        notfound: resolve(__dirname, "src/404.html"),
+      },
+    },
+  },
   assetsInclude: [
     "**/*.png",
     "**/*.jpg",
-    "**/*.jpeg, **/*.PNG, **/*.JPG, **/*.JPEG",
+    "**/*.jpeg",
+    "**/*.PNG",
+    "**/*.JPG",
+    "**/*.JPEG",
   ],
   server: {
     port: 3000,
